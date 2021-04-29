c5664efd5d4e54f8af8d54d9d24eb2d1a0bd1900
Edson Cizeski <cizeskiedson@gmail.com>
2021-04-29 16:02:56 -0300

    one more measure implemented

--- src/trab.rkt
+++ src/trab.rkt
@@ -1,12 +1,27 @@
 #lang racket
 (require 2htdp/batch-io)
 
-;;funcao que analisa a metrica do numero de comentarios no arquivo
-(define (calcula-comentarios arq)
-  (soma-todos (map acha_comentario arq))
+;;funcao que analisa a metrica do numero de imports do arquivo
+(define (calcula-imports arq)
+  (soma-todos (map acha-imports arq))
   )
 
-(define (acha_comentario string)
+(define (acha-imports string)
+  (cond
+    [(string-prefix? string "(require") 1]
+    [else 0]
+    )
+  )
+
+;;funcao que analisa a metrica do numero de comentarios no arquivo
+(define (calcula-comentarios arq)
+  (soma-todos (map acha-comentario arq))
+  )
+
+(define (soma-todos lista)
+ (foldr + 0 lista))
+
+(define (acha-comentario string)
   (cond
     [(string-prefix? string ";") 1]
     [(string-prefix? string "#;") 1]
@@ -26,8 +41,30 @@
     [else 0]
   ))
 
-(define (soma-todos lista)
- (foldr + 0 lista))
+;;funcao que analisa a metrica do numero de linhas de uma funcao do arquivo
+(define (conta-linhas arq indices_funcoes)
+  (for/list([i indices_funcoes])
+    (cond [(equal? (length indices_funcoes) 1) (- (length arq) i)]
+          [else (lambda() (percorre-funcao(split-at arq i)))])
+          )
+  )
+
+(define (linhas-funcao arq)
+  (conta-linhas arq (indexes-of(map acha-funcoes arq) 1))
+  )
+
+(define (percorre-funcao lista)
+  (map acha-fim lista)
+  )
+
+(define (acha-fim string)
+  (cond
+    [(string-prefix? string "(define") 1]
+    [(equal? (acha-comentario string) 1) 1]
+    [(string-prefix? string "") 1]
+    [else 0]
+  ))
+
 
 ;; funcao que retorna a lista com os valores calculados pelas metricas
 (define (avalia-codigo arq nomeArq)
@@ -36,6 +73,7 @@
              (list (string-append "\nNumero de linhas: " (~v(length arq))) ;;metrica 1 do total de linhas do arquivo
                    (string-append "\nNumero de comentarios: " (~v(calcula-comentarios arq)))
                    (string-append "\nNumero de funcoes: " (~v(calcula-funcoes arq)))
+                   (string-append "\nNumero de imports: " (~v(calcula-imports arq)))
                    (string-append "\n"))
              )
   )
