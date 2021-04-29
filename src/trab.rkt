746b11f92a43978a7198a454dfb758bd1f9c0163
Edson Cizeski <cizeskiedson@gmail.com>
2021-04-29 11:57:21 -0300

    finishing metrics, adding directory list

--- src/trab.rkt
+++ src/trab.rkt
@@ -3,7 +3,7 @@
 
 ;;funcao que analisa a metrica do numero de comentarios no arquivo
 (define (calcula-comentarios arq)
-  (map acha_comentario arq)
+  (soma-todos (map acha_comentario arq))
   )
 
 (define (acha_comentario string)
@@ -14,16 +14,37 @@
     [(string-prefix? string "#|") 1]
     [else 0]
   ))
+
+;;funcao que analisa a metrica do numero de funcoes do arquivo
+(define (calcula-funcoes arq)
+  (soma-todos (map acha-funcoes arq))
+  )
+
+(define (acha-funcoes string)
+  (cond
+    [(string-prefix? string "(define") 1]
+    [else 0]
+  ))
+
+(define (soma-todos lista)
+ (foldr + 0 lista))
+
 ;; funcao que retorna a lista com os valores calculados pelas metricas
 (define (avalia-codigo arq nomeArq)
   (printf "\nCodigo analisado: ~a\n" nomeArq)
   (printf "\nMetricas calculadas: ~a"
              (list (string-append "\nNumero de linhas: " (~v(length arq))) ;;metrica 1 do total de linhas do arquivo
-                   (string-append "\nNumero de comentarios: " "teste")
-                   (string-append "\nNumero de funcoes: " "teste2"))
+                   (string-append "\nNumero de comentarios: " (~v(calcula-comentarios arq)))
+                   (string-append "\nNumero de funcoes: " (~v(calcula-funcoes arq)))
+                   (string-append "\n"))
              )
   )
 
 ;; funcao que le os arquivos
-(define (le-arquivos arq)
-  (avalia-codigo(read-lines arq) arq))            
+(define (le-arquivos)
+  (let ([arquivos (map path->string (directory-list "files/" #:build? #t))])
+  (map (lambda (arq) (avalia-codigo (read-lines arq) (string-replace arq "/files" ""))) arquivos)
+ ))
+
+(define (get-nome string)
+  (string-replace string "/files" ""))
